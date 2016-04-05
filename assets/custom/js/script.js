2dd636cf921c364f289072ff1612db6a6294e8c7
Thibaut Tiberghien <thibaut@planecq.com>
2016-04-05 14:18:38 +0800

    Add page-scroll actions to menu, with jquery easing

--- assets/custom/js/script.js
+++ assets/custom/js/script.js
@@ -306,10 +306,23 @@ jQuery(document).ready(function () {
   jQuery('#back-top a').click(function () {
     $('body,html').animate({
       scrollTop: 0
-    }, 600);
+    }, 1200, 'easeInOutExpo');
     return false;
   });
 
+
+  /* Page scroll links */
+
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1200, 'easeInOutExpo');
+      event.preventDefault();
+    });
+  });
 
 
   /* Flickr feed */
