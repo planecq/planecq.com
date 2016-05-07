61e5c8caeefb63a0f9d5bf03f57cec14367dad72
Thibaut Tiberghien <thibaut@planecq.com>
2016-05-08 04:38:16 +0800

    Add travis deploy of staging branch

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,10 +13,27 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     git config user.email "travis@planecq.com"
 
     # commit compressed files and push it to remote
+    touch .deploy
     git add .
     git status # debug
     git commit -m "Deploy compressed files"
-    git push -f deploy master
+    git push -f deploy $TRAVIS_BRANCH:master
+
+elif [ $TRAVIS_BRANCH == "staging" ] ; then
+
+    # setup ssh agent, git config and remote
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/travis_rsa
+    git remote add deploy "travis@webhost.planecq.xyz:/var/www/planecq.xyz"
+    git config user.name "Travis CI"
+    git config user.email "travis@planecq.com"
+
+    # commit compressed files and push it to remote
+    touch .deploy
+    git add .
+    git status # debug
+    git commit -m "Deploy compressed files"
+    git push -f deploy $TRAVIS_BRANCH:master
 
 else
 
