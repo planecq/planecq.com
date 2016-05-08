8d8dd947e1c995c740fe0821f8e9e091d64b0f96
Thibaut Tiberghien <thibaut@planecq.com>
2016-05-08 17:23:20 +0800

    Add html proofer as gulp task (and full test task)

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var exec = require('child_process').exec;
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
@@ -14,9 +15,28 @@ gulp.task('sass:watch', function() {
   gulp.watch('./assets/custom/scss/style.scss', ['sass']);
 });
 
+// Validates html, links, etc.
+gulp.task('html-proofer', function(done) {
+  execute('htmlproofer ./index.html --check-html --check-favicon --check-external-hash', {}, done);
+});
+
+// Validate bootstrap
 gulp.task('bootlint', function() {
   return gulp.src('./index.html')
     .pipe(bootlint({
       stoponerror: true
     }));
 });
+
+// Full test task
+gulp.task('test', ['html-proofer', 'bootlint']);
+
+// Util to execute external command
+function execute(cmd, opts, done) {
+  console.log(cmd);
+  exec(cmd, opts, function(error, stdout, stderr) {
+    console.log(stdout);
+    console.error(stderr);
+    done(error);
+  });
+}
