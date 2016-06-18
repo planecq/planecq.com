44d30163ec20c67435d554642c5b2c7f35bcb260
Thibaut Tiberghien <thibaut@planecq.com>
2016-06-18 16:59:39 +0800

    Master deploy match staging deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,7 +13,8 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     git config user.email "travis@planecq.com"
 
     # commit compressed files and push it to remote
-    touch .deploy
+    rm -f .gitignore
+    cp .travis/deployignore .gitignore
     git add .
     git status # debug
     git commit -m "Deploy compressed files"
